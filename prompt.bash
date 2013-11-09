e6c24bb1218e3ff4f2d29e371146743b80aaa6e3
Chris Thorn <thorncp@gmail.com>
2013-11-09 13:07:20 -0900

    Speed up git branch name in prompt
    Not sure yet how robust this solution is, with handling rebase status,
    for example. Will find out soon enough.

--- prompt.bash
+++ prompt.bash
@@ -3,12 +3,8 @@ wrap_exit_status() {
   echo "\`if [[ \$? = '0' ]]; then echo '$IGreen'; else echo '$IRed'; fi\`$1$Color_Off"
 }
 
-function parse_git_dirty {
-  [[ $(git status 2> /dev/null | tail -n1) != *"working directory clean"* ]] && echo "*"
-}
-
 function parse_git_branch {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/$(parse_git_dirty)\1/"
+  [ -e .git/HEAD ] && cat .git/HEAD | cut -d / -f3-
 }
 
 export PS1="$(wrap_exit_status '\w') $IYellow\$(parse_git_branch)$Color_Off\n\$ "
