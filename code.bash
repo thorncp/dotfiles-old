eedfa5c6d1e2811ac7a1ab6407a1c28376262260
Chris Thorn <thorncp@gmail.com>
2014-03-24 09:15:30 -0700

    Use selecta for code function
    The previous implementation's tab completion did not handle periods in
    directory names. Also, I think tab completion was subpar solution for
    picking from a set of directories. My code directory inevitably accrues
    lots of child directories with similar names, which makes tab completion
    near worthless.

--- code.bash
+++ code.bash
@@ -1,30 +1,8 @@
-# shortcut to cd into ~/code with cd-like tab completion
 code() {
-  cd ~/code/$1
-}
-
-_code() {
-  # here be dragons
-  # TODO: find/learn what utils will help me here
-
-  local cur opts dir base
-  cur="${COMP_WORDS[COMP_CWORD]}"
-
-  # this is essentially a tweak of the dirname utility. it needs to treat a /
-  # as a path element. example: `dirname project/` will return ".", but it is
-  # more useful in this situation to return "project"
-  if [[ "${cur}" == */* ]]; then
-    dir="$(echo ${cur} | sed 's/\/[^\/]*$//')/"
+  if [ $# -eq 0 ]; then
+    cd $(find ~/code -maxdepth 2 -type d | selecta)
   else
-    dir=""
+    cd $(find ~/code -maxdepth 2 -type d | selecta -s "$@")
   fi
-
-  # a tweak of the basename utility. it needs to treat a / as a path element.
-  # example: `basename project/` returns "project", but it is more useful in
-  # this situation to return an empty string
-  base=$(echo ${cur} | sed 's/^\([^\/]*\/\)*//')
-
-  opts=$(cd ~/code/${dir} ; ls -d */. | sed 's/\.//')
-  COMPREPLY=($(compgen -P "${dir}" -W "${opts}" -- ${base}))
+  clear
 }
-complete -o nospace -F _code code
